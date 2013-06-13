1f244b973c90078e07ef14ba3daea73ea7c58f56
Daniel Marques <dmarques.bh@gmail.com>
2013-06-13 18:35:11 -0300

    Navegação em páginas públicas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 FirstApp::Application.routes.draw do
  
+  get "user/teste"
+
   root :to => 'welcome#index', :as => :index
   match 'about' => 'welcome#about', :as => :about
   match 'contact' => 'welcome#contact', :as => :contact
