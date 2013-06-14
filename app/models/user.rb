2fb3c9b1cf866b41d1e841801be1dd0dcb6f86da
Daniel Marques <dmarques.bh@gmail.com>
2013-06-13 22:19:44 -0300

    Não permitir que sejam criados usuários com nome ou email em branco.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :age
+
+  validates :name, :presence => true, :allow_blank => false
+  validates :email, :presence => true, :allow_blank => false
 end
