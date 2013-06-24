610c7af8986188063eb8401c0cf61e877015394f
Daniel Marques <dmarques.bh@gmail.com>
2013-06-24 20:43:32 -0300

    Uso de bootstrap alert

---
+++ app/helpers/application_helper_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelperHelper
+  def bootstrap_alert(type)
+    html = <<-HTML
+      <div class="alert #{ type }">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        #{notice}
+      </div>
+    HTML
+    html.html_safe
+  end
+end
